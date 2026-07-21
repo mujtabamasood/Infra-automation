1248413590da19630637f167014958a2ad994686
Mujtaba Masood Ali <65421710+mujtabamasood@users.noreply.github.com>
2026-07-20 19:43:24 -0500

    Delete github_starter directory

--- github_starter/terraform/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "aws_region"    { type = string  default = "us-east-1" }
-variable "project"       { type = string  default = "demo" }
-variable "environment"   { type = string  default = "dev" }
-variable "vpc_cidr"      { type = string  default = "10.0.0.0/16" }
-variable "subnet_cidr"   { type = string  default = "10.0.1.0/24" }
-variable "instance_type" { type = string  default = "t3.micro" }
-variable "ami_id"        { type = string  default = "ami-0abcdef1234567890" }
